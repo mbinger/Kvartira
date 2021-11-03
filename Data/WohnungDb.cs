746653b0865774ec9a337b9544636784da152a3d
maksim.binger <maksim.binger@vattenfall.de>
2021-11-03 07:52:40 +0100

    Add model

---
+++ Data/WohnungDb.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Data
+{
+    public class WohnungDb: DbContext
+    {
+        public virtual DbSet<WohnungHeader> WohnungHeaders { get; set; }
+        public virtual DbSet<WohnungDetails> WohnungDetails { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Filename=Data\\WohnungDb.sqlite");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<WohnungHeader>().HasIndex(p => new { p.Provider, p.WohnungId }).IsUnique();
+        }
+    }
+}
---
+++ Data/WohnungHeader.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Data
+{
+    public class WohnungHeader
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [StringLength(50)]
+        public string Provider { get; set; }
+
+        [StringLength(256)]
+        public string WohnungId { get; set; }
+
+        public bool TraumWohnung { get; set; }
+        
+        public bool Gesehen { get; set; }
+
+        public bool Gemeldet { get; set; }
+
+        public virtual ICollection<WohnungDetails> Details { get; set; }
+    }
+}
